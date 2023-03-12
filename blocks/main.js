3b309d61d88441431f43d020c3519aae393a0e32
alkn203 <alkn203@gmail.com>
2023-03-13 00:58:08 +0900

    update

--- blocks/main.js
+++ blocks/main.js
@@ -1,140 +1,211 @@
+// 型定義ファイルを参照
+/// <reference path="../node_modules/phina.js.d.ts/globalized/index.d.ts" />
 phina.globalize();
 // 定数
-const BLOCK_SIZE = 40
-const BLOCK_OFFSET = BLOCK_SIZE / 2;
-const BLOCK_COLS = 10
-const BLOCK_ROWS = 20
-const BLOCK_NUMS = 4
-const BLOCK_TYPE = 7
-const BOTTOM_Y = 20
-const EDGE_LEFT = 2
-const EDGE_RIGHT = 13
-const INTERVAL = 1000;
+var BLOCK_SIZE = 40;
+var BLOCK_OFFSET = BLOCK_SIZE / 2;
+var BLOCK_COLS = 10;
+var BLOCK_ROWS = 20;
+var BLOCK_NUMS = 4;
+var BLOCK_TYPE = 7;
+var BOTTOM_Y = 20;
+var EDGE_LEFT = 2;
+var EDGE_RIGHT = 13;
+var INTERVAL = 1000;
 // アセット
-const ASSETS = {
-  // 画像
-  image: {
-    'block': 'assets/block.png',
-  },
+var ASSETS = {
+    // 画像
+    image: {
+        'block': 'assets/block.png'
+    }
 };
 // ブロック(7種)の配置情報
-const BLOCK_LAYOUT = [
-  [Vector2(0, 0), Vector2(0, -1), Vector2(0, -2), Vector2(0, 1)],
-  [Vector2(0, 0), Vector2(0, -1), Vector2(0, 1), Vector2(1, 1)],
-  [Vector2(0, 0), Vector2(0, -1), Vector2(0, 1), Vector2(-1, 1)],
-  [Vector2(0, 0), Vector2(0, -1), Vector2(-1, -1), Vector2(1, 0)],
-  [Vector2(0, 0), Vector2(0, -1), Vector2(1, -1), Vector2(-1, 0)],
-  [Vector2(0, 0), Vector2(1, 0), Vector2(-1, 0), Vector2(0, -1)],
-  [Vector2(0, 0), Vector2(0, -1), Vector2(1, -1), Vector2(1, 0)]]
+var BLOCK_LAYOUT = [
+    [Vector2(0, 0), Vector2(0, -1), Vector2(0, -2), Vector2(0, 1)],
+    [Vector2(0, 0), Vector2(0, -1), Vector2(0, 1), Vector2(1, 1)],
+    [Vector2(0, 0), Vector2(0, -1), Vector2(0, 1), Vector2(-1, 1)],
+    [Vector2(0, 0), Vector2(0, -1), Vector2(-1, -1), Vector2(1, 0)],
+    [Vector2(0, 0), Vector2(0, -1), Vector2(1, -1), Vector2(-1, 0)],
+    [Vector2(0, 0), Vector2(1, 0), Vector2(-1, 0), Vector2(0, -1)],
+    [Vector2(0, 0), Vector2(0, -1), Vector2(1, -1), Vector2(1, 0)]
+];
 // キー用配列
-const KEY_ARRAY = [
-  ["left", Vector2.LEFT],
-  ["right", Vector2.RIGHT]]
-// メインシーン
+var KEY_ARRAY = [
+    ["left", Vector2.LEFT],
+    ["right", Vector2.RIGHT]
+];
+/**
+ * メインシーン
+ */
 phina.define('MainScene', {
-  superClass: 'DisplayScene',
-  // コンストラクタ
-  init: function() {
-    // 親クラス初期化
-    this.superInit();
-    // 背景色
-    this.backgroundColor = 'gray';
-    // ブロック移動領域
-    var blockArea = RectangleShape({
-      width: BLOCK_SIZE * BLOCK_COLS,
-      height: BLOCK_SIZE * BLOCK_ROWS,
-      fill: 'black',
-    }).addChildTo(this);
-    blockArea.x = this.gridX.center();
-    blockArea.top = 0;
-    // グループ 
-    this.dynamicGroup = DisplayElement().addChildTo(this);
-    // 変数
-    this.prevTime = 0;
-    this.curTime = 0;
-    this.interval = INTERVAL;
-    this.removeLine = [];
-    // ブロック作成
-    this.createBlock();
-  },
-  // 毎フレーム処理
-  update: function(app) {
-    this.curTime += app.deltaTime;
-    // 一定時間毎にブロック落下
-    if (this.curTime - this.prevTime > this.interval) {
-      this.moveBlockY();
-      this.prevTime = this.curTime;
+    superClass: 'DisplayScene',
+    // コンストラクタ
+    init: function () {
+        // 親クラス初期化
+        this.superInit();
+        // 背景色
+        this.backgroundColor = 'gray';
+        // ブロック移動領域
+        var blockArea = RectangleShape({
+            width: BLOCK_SIZE * BLOCK_COLS,
+            height: BLOCK_SIZE * BLOCK_ROWS,
+            fill: 'black'
+        }).addChildTo(this);
+        blockArea.x = this.gridX.center();
+        blockArea.top = 0;
+        // グループ 
+        this.dynamicGroup = DisplayElement().addChildTo(this);
+        this.staticGroup = DisplayElement().addChildTo(this);
+        // 変数
+        this.prevTime = 0;
+        this.curTime = 0;
+        this.interval = INTERVAL;
+        this.removeLine = [];
+        // ブロック作成
+        this.createBlock();
+    },
+    /**
+     * 毎フレーム処理
+     */
+    update: function (app) {
+        this.curTime += app.deltaTime;
+        // 一定時間毎にブロック落下
+        if (this.curTime - this.prevTime > this.interval) {
+            this.moveBlockY();
+            this.prevTime = this.curTime;
+        }
+    },
+    /**
+     * ブロック作成関数
+     */
+    createBlock: function () {
+        // 種類をランダムに決める
+        var type = Random.randint(0, BLOCK_TYPE - 1);
+        // 落下ブロック作成
+        for (var i = 0; i < 4; i++) {
+            //@ts-ignore
+            var block = Block().addChildTo(this.dynamicGroup);
+            // ブロックの種類
+            block.type = type;
+            // フレームインデックス設定
+            block.frameIndex = type;
+        }
+        // 中心ブロック
+        var org = this.dynamicGroup.children.first;
+        org.x = this.gridX.center() + BLOCK_OFFSET;
+        org.y = 0;
+        // 配置情報データをもとにブロックを配置
+        for (var i = 0; i < 4; i++) {
+            var block = this.dynamicGroup.children[i];
+            block.x = org.x + BLOCK_LAYOUT[type][i].x * BLOCK_SIZE;
+            block.y = org.y + BLOCK_LAYOUT[type][i].y * BLOCK_SIZE;
+            block.indexPos = this.coordToIndex(block.position);
+        }
+    },
+    /**
+     * ブロック落下処理
+     */
+    moveBlockY: function () {
+        // 1ブロック分落下
+        this.moveBlock(Vector2.DOWN);
+        // 画面下到達か固定ブロックにヒット
+        if (this.hitBottom() && this.hitStatic()) {
+            // ブロックを戻す
+            this.moveBlock(Vector2.UP);
+            // 固定ブロックへ追加
+            this.dynamicToStatic();
+        }
+    },
+    /**
+     * ブロック移動処理
+     */
+    moveBlock: function (vec) {
+        var children = this.dynamicGroup.children;
+        var len = children.length;
+        for (var i = 0; i < len; i++) {
+            var block = this.dynamicGroup.children[i];
+            block.position.add(Vector2.mul(vec, BLOCK_SIZE));
+            block.indexPos.add(vec);
+        }
+    },
+    /**
+     * 画面下到達チェック
+     */
+    hitBottom: function () {
+        var children = this.dynamicGroup.children;
+        var len = children.length;
+        for (var i = 0; i < len; i++) {
+            var block = children[i];
+            if (block.indexPos.y === BOTTOM_Y) {
+                return true;
+            }
+        }
+        return false;
+    },
+    /**
+     * 固定ブロックとの当たり判定
+     */
+    hitStatic: function () {
+        var children = this.dynamicGroup.children;
+        var len = children.length;
+        var children2 = this.staticGroup.children;
+        var len2 = children2.length;
+        for (var i = 0; i < len; i++) {
+            var block = children[i];
+            for (var j = 0; j < len2; j++) {
+                var target = children2[j];
+                // 位置が一致したら
+                if (block.indexPos.equals(target.indexPos)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+    /**
+     * 移動ブロックから固定ブロックへの変更処理
+     */
+    dynamicToStatic: function () {
+        // グループ間の移動
+        var children = this.dynamicGroup.children;
+        var len = children.length;
+        for (var i = 0; i < len; i++) {
+            children.pop().addChildTo(this.staticGroup);
+        }
+    },
+    /**
+     * 座標値からインデックス値へ変換
+     */
+    coordToIndex: function (vec) {
+        var x = Math.floor(vec.x / BLOCK_SIZE);
+        var y = Math.floor(vec.y / BLOCK_SIZE);
+        return Vector2(x, y);
     }
-  },
-  // ブロック作成関数
-  createBlock: function() {
-    // 種類をランダムに決める
-    const type = Random.randint(0, BLOCK_TYPE - 1);
-    // 落下ブロック作成
-    const num = BLOCK_NUM;
-    num.times(function() {
-      const block = Block().addChildTo(this.dynamicGroup);
-      // ブロックの種類
-      block.type = type;
-      // フレームインデックス設定
-      block.frameIndex = type;
-    }, this);
-    // 中心ブロック
-    var org = this.dynamicGroup.children.first;
-    org.x = this.gridX.center() + BLOCK_OFFSET;
-    org.y = 0;
-    // 配置情報データをもとにブロックを配置
-    this.dynamicGroup.children.each(function(block, i) {
-      block.x = org.x + BLOCK_LAYOUT[type][i].x * BLOCK_SIZE;
-      block.y = org.y + BLOCK_LAYOUT[type][i].y * BLOCK_SIZE;
-      block.indexPos = this.coordToIndex(block.position);
-    }, this);
-  },
-  // ブロック落下処理
-  moveBlockY: function() {
-    // 1ブロック分落下
-    this.moveBlock(Vector2.DOWN);
-/*    // 画面下到達か固定ブロックにヒット
-    if _hit_bottom() or _hit_static():
-      # ブロックを戻す
-      _move_block(Vector2.UP)
-      # 固定ブロックへ追加
-      _dynamic_to_static()
-      #
-      _check_remove_line()*/
-  },
-  // ブロック移動処理
-  moveBlock: function(vec) {
-    this.dynamicGroup.children.each(function(block) {
-      block.position.add(Vector2.mul(vec, BLOCK_SIZE));
-      block.indexPos.add(vec);
-    });  
-  },
-  // 座標値からインデックス値へ変換
-  coordToIndex: function(vec) {
-    const x = Math.floor(vec.x / BLOCK_SIZE);
-    const y = Math.floor(vec.y / BLOCK_SIZE);
-    return Vector2(x, y);
-  },
 });
-// ブロッククラス
+/**
+ * ブロッククラス
+ */
 phina.define('Block', {
-  // Spriteを継承
-  superClass: 'Sprite',
-  // コンストラクタ
-  init: function() {
-    // 親クラス初期化
-    this.superInit('block', BLOCK_SIZE, BLOCK_SIZE);
-    // 位置インデックス
-    this.indexPos = Vector2.ZERO;
-  },
+    // Spriteを継承
+    superClass: 'Sprite',
+    // コンストラクタ
+    init: function () {
+        // 親クラス初期化
+        this.superInit('block', BLOCK_SIZE, BLOCK_SIZE);
+        // 位置インデックス
+        this.indexPos = Vector2.ZERO;
+        // ブロックの種類
+        this.type = 0;
+    }
 });
-// メイン
+/**
+ * メイン
+ */
 phina.main(function () {
-  const app = GameApp({
-    startLabel: 'main',
-    // アセット読み込み
-    assets: ASSETS,
-  });
-  app.run();
+    var app = GameApp({
+        startLabel: 'main',
+        // アセット読み込み
+        assets: ASSETS
+    });
+    app.run();
 });
---
+++ blocks/main.ts
@@ -0,0 +1,222 @@
+// 型定義ファイルを参照
+/// <reference path="../node_modules/phina.js.d.ts/globalized/index.d.ts" />
+
+phina.globalize();
+// 定数
+const BLOCK_SIZE = 40
+const BLOCK_OFFSET = BLOCK_SIZE / 2;
+const BLOCK_COLS = 10
+const BLOCK_ROWS = 20
+const BLOCK_NUMS = 4
+const BLOCK_TYPE = 7
+const BOTTOM_Y = 20
+const EDGE_LEFT = 2
+const EDGE_RIGHT = 13
+const INTERVAL = 1000;
+// アセット
+const ASSETS = {
+  // 画像
+  image: {
+    'block': 'assets/block.png',
+  },
+};
+// ブロック(7種)の配置情報
+const BLOCK_LAYOUT = [
+  [Vector2(0, 0), Vector2(0, -1), Vector2(0, -2), Vector2(0, 1)],
+  [Vector2(0, 0), Vector2(0, -1), Vector2(0, 1), Vector2(1, 1)],
+  [Vector2(0, 0), Vector2(0, -1), Vector2(0, 1), Vector2(-1, 1)],
+  [Vector2(0, 0), Vector2(0, -1), Vector2(-1, -1), Vector2(1, 0)],
+  [Vector2(0, 0), Vector2(0, -1), Vector2(1, -1), Vector2(-1, 0)],
+  [Vector2(0, 0), Vector2(1, 0), Vector2(-1, 0), Vector2(0, -1)],
+  [Vector2(0, 0), Vector2(0, -1), Vector2(1, -1), Vector2(1, 0)]]
+// キー用配列
+const KEY_ARRAY = [
+  ["left", Vector2.LEFT],
+  ["right", Vector2.RIGHT]]
+// 自作クラス補完用
+interface Block extends Sprite {
+  indexPos: Vector2
+  type: number
+}
+/**
+ * メインシーン
+ */
+phina.define('MainScene', {
+  superClass: 'DisplayScene',
+  // コンストラクタ
+  init: function() {
+    // 親クラス初期化
+    this.superInit();
+    // 背景色
+    this.backgroundColor = 'gray';
+    // ブロック移動領域
+    var blockArea = RectangleShape({
+      width: BLOCK_SIZE * BLOCK_COLS,
+      height: BLOCK_SIZE * BLOCK_ROWS,
+      fill: 'black',
+    }).addChildTo(this);
+    blockArea.x = this.gridX.center();
+    blockArea.top = 0;
+    // グループ 
+    this.dynamicGroup = DisplayElement().addChildTo(this);
+    this.staticGroup = DisplayElement().addChildTo(this);
+    // 変数
+    this.prevTime = 0;
+    this.curTime = 0;
+    this.interval = INTERVAL;
+    this.removeLine = [];
+    // ブロック作成
+    this.createBlock();
+  },
+  /**
+   * 毎フレーム処理
+   */
+  update: function(app: Ticker) {
+    this.curTime += app.deltaTime;
+    // 一定時間毎にブロック落下
+    if (this.curTime - this.prevTime > this.interval) {
+      this.moveBlockY();
+      this.prevTime = this.curTime;
+    }
+  },
+  /**
+   * ブロック作成関数
+   */
+  createBlock: function() {
+    // 種類をランダムに決める
+    const type: number = Random.randint(0, BLOCK_TYPE - 1);
+    // 落下ブロック作成
+    for (let i = 0; i < 4; i++) {
+      //@ts-ignore
+      const block: Block = Block().addChildTo(this.dynamicGroup);
+      // ブロックの種類
+      block.type = type;
+      // フレームインデックス設定
+      block.frameIndex = type;
+    }
+    // 中心ブロック
+    var org: Block = this.dynamicGroup.children.first;
+    org.x = this.gridX.center() + BLOCK_OFFSET;
+    org.y = 0;
+    // 配置情報データをもとにブロックを配置
+    for (let i = 0; i < 4; i++) {
+      const block: Block = this.dynamicGroup.children[i];
+      block.x = org.x + BLOCK_LAYOUT[type][i].x * BLOCK_SIZE;
+      block.y = org.y + BLOCK_LAYOUT[type][i].y * BLOCK_SIZE;
+      block.indexPos = this.coordToIndex(block.position);
+    }
+  },
+  /**
+   * ブロック落下処理
+   */
+  moveBlockY: function() {
+    // 1ブロック分落下
+    this.moveBlock(Vector2.DOWN);
+    // 画面下到達か固定ブロックにヒット
+    if (this.hitBottom() && this.hitStatic()) {
+      // ブロックを戻す
+      this.moveBlock(Vector2.UP);
+      // 固定ブロックへ追加
+      this.dynamicToStatic();
+    }
+
+  },
+  /**
+   * ブロック移動処理
+   */
+  moveBlock: function(vec: Vector2) {
+    const children: Block[] = this.dynamicGroup.children;
+    const len: number = children.length; 
+
+    for (let i = 0; i < len; i++) {
+      const block: Block = this.dynamicGroup.children[i];
+      block.position.add(Vector2.mul(vec, BLOCK_SIZE));
+      block.indexPos.add(vec);
+    }
+  },
+  /**
+   * 画面下到達チェック
+   */
+  hitBottom: function(): boolean {
+    const children: Block[] = this.dynamicGroup.children;
+    const len: number = children.length;
+
+    for (let i = 0; i < len; i++) {
+      const block: Block = children[i];
+      
+      if (block.indexPos.y === BOTTOM_Y) {
+        return true
+      }
+    }
+    return false
+  },
+  /**
+   * 固定ブロックとの当たり判定
+   */
+  hitStatic: function() {
+    const children: Block[] = this.dynamicGroup.children;
+    const len: number = children.length;
+    const children2: Block[] = this.staticGroup.children;
+    const len2: number = children2.length;
+
+    for (let i = 0; i < len; i++) {
+      const block: Block = children[i];
+
+      for (let j = 0; j < len2; j++) {
+        const target: Block = children2[j];
+        // 位置が一致したら
+        if (block.indexPos.equals(target.indexPos)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
+  /**
+   * 移動ブロックから固定ブロックへの変更処理
+   */
+  dynamicToStatic: function() {
+    // グループ間の移動
+    const children: Block[] = this.dynamicGroup.children;
+    const len: number = children.length;
+
+    for (let i = 0; i < len; i++) {
+      children.pop().addChildTo(this.staticGroup);
+    }
+  },
+  /**
+   * 座標値からインデックス値へ変換
+   */
+  coordToIndex: function(vec: Vector2): Vector2 {
+    const x: number = Math.floor(vec.x / BLOCK_SIZE);
+    const y: number = Math.floor(vec.y / BLOCK_SIZE);
+    return Vector2(x, y);
+  },
+});
+/**
+ * ブロッククラス
+ */
+phina.define('Block', {
+  // Spriteを継承
+  superClass: 'Sprite',
+  // コンストラクタ
+  init: function() {
+    // 親クラス初期化
+    this.superInit('block', BLOCK_SIZE, BLOCK_SIZE);
+    // 位置インデックス
+    this.indexPos = Vector2.ZERO;
+    // ブロックの種類
+    this.type = 0;
+  },
+});
+/**
+ * メイン
+ */
+phina.main(function () {
+  const app = GameApp({
+    startLabel: 'main',
+    // アセット読み込み
+    assets: ASSETS,
+  });
+  app.run();
+});
